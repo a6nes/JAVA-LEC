ccdc473a55bdda3aace0bac84914b5543566f92b
a6nes <kimjong2000@gmail.com>
2024-03-20 12:51:23 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("안녕하세요!");
+        System.out.print("하이");
+        System.out.println("하이");
+        System.out.print("숫자 "+" 3 "+" 과 문자열의 연결도 가능합니다.");
+    }
+}
